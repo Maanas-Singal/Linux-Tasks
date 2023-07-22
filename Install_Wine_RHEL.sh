a6edefed4419cd44a26b6559593478c94264c2ae
Maanas-Singal <139725336+Maanas-Singal@users.noreply.github.com>
2023-07-22 21:09:49 +0530

    Create Install_Wine_RHEL.sh
    This Bash Script installs, and configures the Wine software for RHEL Distro.

---
+++ Install_Wine_RHEL.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root."
+  exit 1
+fi
+
+# Install required dependencies
+dnf install -y dnf-plugins-core
+
+# Enable the CodeReady Linux Builder repository for RHEL 8 (required for Wine)
+if [ -f /etc/redhat-release ] && grep -qi "release 8" /etc/redhat-release; then
+  dnf config-manager --set-enabled codeready-builder-for-rhel-8
+fi
+
+# Install Wine and Winetricks
+dnf install -y wine winetricks
+
+# Run Wine configuration for the current user
+sudo -u $(logname) winecfg
+
+# Optionally, you can install PlayOnLinux for a user-friendly Wine frontend
+read -p "Do you want to install PlayOnLinux? (y/n): " install_pol
+if [ "$install_pol" == "y" ]; then
+  dnf install -y playonlinux
+fi
+
+echo "Wine installation and configuration completed successfully!"
+
+### Before running the script, make sure to give execute permissions to the script:
+### chmod +x install_wine.sh
